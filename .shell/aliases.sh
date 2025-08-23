5e8477f6019f5b1c7c9b4bba19a1ba83bba25a7d
Jonathan Russ <jonathan.russ@bluewin.ch>
2025-08-23 12:28:29 +0200

    refactor: add path.sh & reorganize other files

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -1,41 +1,121 @@
 #!/usr/bin/env bash
 
-if cmd:exists xclip; then
-  alias xc="xclip -selection c"
-  alias xp="xclip -selection clipboard -o"
-elif cmd:exists termux-clipboard-get; then
-  alias xc="termux-clipboard-get"
-  alias xp="termux-clipboard-set"
-fi
+# ---------------------------- Filesystem & Navigation ----------------------- #
 
+# Make the dirs command useful
+alias dirs='dirs -v'
+
+# Color ls
 cmd:exists lsd && alias ls='lsd'
-cmd:exists btop && alias top='btop'
-cmd:exists bat && alias cat='bat'
 
-if cmd:exists nvim; then
-  alias vi=nvim
-  alias vim=nvim
+# --------------------------------- Safety ----------------------------------- #
+
+# Safer rm
+# This should not interfere with your shell scripts, because IMO most of them
+# use either bash or sh. And you should do the same! Make scripts for the
+# least common denominator!
+if cmd:exists trash-put; then
+  alias rm='echo "Please use trash-put"'
 fi
 
-alias python='python3'
-alias dirs="dirs -v"
+# ------------------------------- System Tools ------------------------------- #
+
+# Don't see any reason why we should not use a better top utility if it exists
+cmd:exists btop && alias top='btop'
+
+# Use cat on steroids if it exists, and don't page, like cat does
+# If you don't like the paging behaviour use bat directly
+cmd:exists bat && alias cat='bat --paging=never'
+
+# For enabling lazy loading of ssh keys
 alias ssh='ssh -o AddKeysToAgent=yes'
-alias bash="PERMIT_BASH=true bash"
 
-alias gatekeeper:disable="sudo spctl --master-disable"
-alias gatekeeper:enable="sudo spctl --master-enable"
+# Use Mosh instead of SSH
+# cmd:exists mosh && alias ssh='mosh'
 
-alias ez="\${EDITOR} \${HOME}/.zshrc"
-alias eb="\${EDITOR} \${HOME}/.bashrc"
-alias ef="\${EDITOR} \${HOME}/.shell/func.sh"
-alias ev="\${EDITOR} \${HOME}/.shell/vars.sh"
-alias ea="\${EDITOR} \${HOME}/.shell/aliases.sh"
-alias e="\${EDITOR} ."
+# Make using bash interactively possible
+alias bash='PERMIT_BASH=true bash'
 
-alias -s go="\${EDITOR}"
-alias -s md="\${EDITOR}"
-alias -s yaml="\${EDITOR}"
-alias -s yml="\${EDITOR}"
-alias -s js="\${EDITOR}"
-alias -s ts="\${EDITOR}"
-alias -s json="\${EDITOR}"
+# ------------------------------ Editor & Config ----------------------------- #
+
+# Make sure we make an alias from vi & vim to nvim here, because a symlink might
+# not be installed automatically for nvim. Usually use update-alternatives for
+# debian based systems. If not then we'll set up aliases here:
+if [[ ! -L "$(which "${EDITOR}")" ]]; then
+  if cmd:exists nvim.appimage; then
+    alias vi=nvim.appimage
+    alias vim=nvim.appimage
+    alias nvim=nvim.appimage
+  elif cmd:exists nvim; then
+    alias vi=nvim
+    alias vim=nvim
+  elif cmd:exists vim; then
+    alias vi=vim
+    alias vim=vim
+  fi
+fi
+
+# Edit shell config
+alias es='$EDITOR ${HOME}/.zshrc'
+if [[ -L "${HOME}/.zshrc" ]] && cmd:exists readlink && cmd:exists dirname; then
+  alias es='$EDITOR $(dirname $(readlink -f "${HOME}"/.zshrc))'
+fi
+
+# Edit variables
+alias ev='$EDITOR ${DOTFILES_REPO_PATH}/.shell/vars.sh'
+
+# Open editor in current directory
+alias e='$EDITOR .'
+
+# ------------------------------------ Git ----------------------------------- #
+
+alias gs="git status"
+alias gca="git commit -a -m"
+alias gau="git remote add upstream"
+alias gaa="git add --all"
+alias gpl="git pull"
+alias gpu="git push"
+alias gps="git push && say You are awesome!"
+alias yolos="git push --force && say That was sneaky!"
+alias yolo="git push --force"
+alias gcl="git clone"
+alias glg="git log"
+alias gst="git shortlog -sn"
+alias gch="git checkout"
+alias gba="git branch -av"
+alias gsl="git stash list"
+alias gsc="git stash clear"
+
+gdb() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: gdb <branch>"
+    return 1
+  fi
+  git branch -d -- "$1" && git push origin :"$1"
+}
+
+gas() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: gas <stash_index>"
+    return 1
+  fi
+  git stash apply "stash@{$1}"
+}
+
+grb() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: grb <commit_count>"
+    return 1
+  fi
+  git rebase -i "HEAD~$1"
+}
+
+# ------------------------------- Suffix alias ------------------------------- #
+
+alias -s go='$EDITOR'
+alias -s md='$EDITOR'
+alias -s yaml='$EDITOR'
+alias -s yml='$EDITOR'
+alias -s js='$EDITOR'
+alias -s ts='$EDITOR'
+alias -s json='$EDITOR'
--- .zprofile
+++ .zprofile
@@ -1,14 +1,5 @@
 # Amazon Q pre block. Keep at the top of this file.
 [[ -f "${HOME}/Library/Application Support/amazon-q/shell/zprofile.pre.zsh" ]] && builtin source "${HOME}/Library/Application Support/amazon-q/shell/zprofile.pre.zsh"
-eval "$(/opt/homebrew/bin/brew shellenv)"
-eval "$(pyenv init --path)"
-
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"                                       # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
-
-# >>> coursier install directory >>>
-export PATH="$PATH:/Users/$USER/Library/Application Support/Coursier/bin"
-# <<< coursier install directory <<<
 
 # Amazon Q post block. Keep at the bottom of this file.
 [[ -f "${HOME}/Library/Application Support/amazon-q/shell/zprofile.post.zsh" ]] && builtin source "${HOME}/Library/Application Support/amazon-q/shell/zprofile.post.zsh"
